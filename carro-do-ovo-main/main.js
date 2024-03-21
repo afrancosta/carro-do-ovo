df55a7564b350d388ddcdcf50d53317a751686e4
afrancosta <129406030+afrancosta@users.noreply.github.com>
2024-03-21 08:07:15 -0300

    Add files via upload
    21/03

--- carro-do-ovo-main/main.js
+++ carro-do-ovo-main/main.js
@@ -1,2 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 console.log(botoes)
+for (let i=0; i <botoes.lenght; i++) {
+    console.log(1);
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        botoes[i].classList.add("ativo");
+    }
+  }
+}
