145c677baa6a78a212a48458bf92b1331e5c0fb6
afrancosta <129406030+afrancosta@users.noreply.github.com>
2024-05-23 09:36:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i=0;i <botoes.lenght; i++) {
      botoes[i].onclick=function(){
@@ -7,6 +8,6 @@ for(let j=0;j <botoes.length;j++){
     botoes[j].classList.remove("ativo");
    }
         botoes[i].classList.add("ativo");
-
+        textos[i].classList.add("ativo");
   }
 }
